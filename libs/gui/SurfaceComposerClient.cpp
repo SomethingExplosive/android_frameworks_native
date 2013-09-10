948a3f1a15385e81c501866646fdc89e8f5334cf
something15525 <something15525@gmail.com>
2013-09-10 14:32:37 -0700

    frameworks/native: fix toroplus LOCAL_CFLAGS usage
    This wasn't being done correctly, here's a fix.

--- libs/gui/SurfaceComposerClient.cpp
+++ libs/gui/SurfaceComposerClient.cpp
@@ -638,13 +638,14 @@ ScreenshotClient::ScreenshotClient()
 ScreenshotClient::~ScreenshotClient() {
     ScreenshotClient::release();
 }
- // TODO: Remove me. Do not use.
- // This is a compatibilty shim for one product whose drivers are depending on
- // this legacy function (when they shouldn't).
+
+#if defined(TOROPLUS_RADIO)
 status_t ScreenshotClient::update() {
     sp<ISurfaceComposer> sm(ComposerService::getComposerService());
     return update(sm->getBuiltInDisplay(0));
 }
+#endif
+
 sp<CpuConsumer> ScreenshotClient::getCpuConsumer() const {
     if (mCpuConsumer == NULL) {
         mCpuConsumer = new CpuConsumer(1);
